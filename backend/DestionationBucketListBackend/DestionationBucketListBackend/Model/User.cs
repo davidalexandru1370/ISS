6d1ecbfe2666e59000e08b2db1175b01d59a9a44
David <fanefaraon696@gmail.com>
2023-03-27 06:26:29 +0300

    created user model and database context

---
+++ backend/DestionationBucketListBackend/DestionationBucketListBackend/Model/User.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DestionationBucketListBackend.Model;
+
+[Table("Users")]
+public class User 
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    [Required(ErrorMessage = "Email cannot be empty!")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Password cannot be empty")]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+}
--- backend/DestionationBucketListBackend/DestionationBucketListBackend/Program.cs
+++ backend/DestionationBucketListBackend/DestionationBucketListBackend/Program.cs
@@ -1,9 +1,15 @@
+using DestionationBucketListBackend.DbContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DestinationBucketDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Dev")));
+
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
