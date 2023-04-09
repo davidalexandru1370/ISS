76d0489f113ffcc26f02da4eaf6d67a001b1247c
David <fanefaraon696@gmail.com>
2023-04-09 19:50:50 +0300

    created register endpoint

--- frontend/src/Api/UserApi.ts
+++ frontend/src/Api/UserApi.ts
@@ -6,3 +6,11 @@ export const login = async (user: User) => {
   let url = UserEndpoints.login;
   return await (await fetch(url, createHeader(Methods.POST, user))).json();
 };
+
+export const register = async (user: User) => {
+  let url = UserEndpoints.register;
+  let response = await (
+    await fetch(url, createHeader(Methods.POST, user))
+  ).json();
+  return await response;
+};
