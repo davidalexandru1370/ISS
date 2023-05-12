ca7003c94a17c42e68596c097f5deaf8d3d78275
davidalexandru1370 <fanefaraon696@gmail.com>
2023-05-12 21:19:19 +0300

    added firebase image upload

--- backend/DestionationBucketListBackend/DestinationBucketListBackend/Controllers/DestinationController.cs
+++ backend/DestionationBucketListBackend/DestinationBucketListBackend/Controllers/DestinationController.cs
@@ -1,25 +1,32 @@
+using System.Text;
 using DestinationBucketListBackend.Exceptions;
 using DestinationBucketListBackend.ExtensionMethods;
 using DestinationBucketListBackend.Model;
 using DestinationBucketListBackend.Model.DTO;
 using DestinationBucketListBackend.Services.Interfaces;
+using Firebase.Auth;
+using Firebase.Storage;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32.SafeHandles;
 
 namespace DestinationBucketListBackend.Controllers;
 
-[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class DestinationController : ControllerBase
 {
     private readonly IDestinationService _destinationService;
+    private static string ApiKey = "AIzaSyBG_DScFgkNUOooWMoGvv2D1C7V85gKg5A";
+    private static string Bucket = "destinationbucketimages.appspot.com";
+    private static string AuthEmail = "admin@gmail.com";
+    private static string AuthPassword = "123456789";
 
     public DestinationController(IDestinationService destinationService)
     {
         _destinationService = destinationService;
     }
-    
+
     [HttpPost]
     [Route("add-destination")]
     public async Task<ActionResult<Destination>> AddDestination([FromBody] DestinationDto destination)
@@ -49,4 +56,51 @@ public class DestinationController : ControllerBase
             return Unauthorized();
         }
     }
+
+    [AllowAnonymous]
+    [HttpPost]
+    [Route("add-image")]
+    public async Task UploadImageToFirebase()
+    {
+        // of course you can login using other method, not just email+password
+        var auth = new FirebaseAuthProvider(new FirebaseConfig(ApiKey));
+        var a = await auth.SignInWithEmailAndPasswordAsync(AuthEmail, AuthPassword);
+
+        // you can use CancellationTokenSource to cancel the upload midway
+
+        var cancellation = new CancellationTokenSource();
+        FileStream stream =
+            System.IO.File.Open(@"windows-xp-desktop-background-wallpaper-azul-800x600.jpg",
+                FileMode.Open);
+        var task = new FirebaseStorage(
+                Bucket,
+                new FirebaseStorageOptions
+                {
+                    AuthTokenAsyncFactory = () => Task.FromResult(a.FirebaseToken),
+                    ThrowOnCancel =
+                        true // when you cancel the upload, exception is thrown. By default no exception is thrown
+                })
+            .Child("images")
+            .PutAsync(stream, cancellation.Token);
+
+        task.Progress.ProgressChanged += (s, e) => Console.WriteLine($"Progress: {e.Percentage} %");
+
+        // cancel the upload
+        // cancellation.Cancel();
+
+        try
+        {
+            // error during upload will be thrown when you await the task
+            Console.WriteLine("Download link:\n" + await task);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception was thrown: {0}", ex);
+        }
+        finally
+        {
+            await stream.DisposeAsync();
+        }
+    }
 }
+
